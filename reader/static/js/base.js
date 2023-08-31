71c19a1ea2aa84e4db68f0649a2da71ef56592d2
unknown <alanuldashev@gmail.com>
2023-08-31 23:39:10 +0600

    second commit

---
+++ reader/static/js/base.js
@@ -0,0 +1,16 @@
+var icon = document.getElementById("icon");
+var icon1 = document.getElementById("a");
+var icon2 = document.getElementById("b");
+var icon3 = document.getElementById("c");
+var nav = document.getElementById('nav');
+var blue = document.getElementById("blue");
+var body_non_scroll = document.querySelector('body')
+
+icon.addEventListener('click', function() {
+  icon1.classList.toggle('a');
+  icon2.classList.toggle('c');
+  icon3.classList.toggle('b');
+  nav.classList.toggle('show');
+  blue.classList.toggle('slide');
+  body_non_scroll.classList.toggle('non_scroll')
+});
