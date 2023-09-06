1054d6e5d8d64d19761ea2749729797f224f47da
unknown <alanuldashev@gmail.com>
2023-09-06 20:13:06 +0600

    manual bookmarks

--- user_actions/urls.py
+++ user_actions/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import search
+from .views import search, bookmark
 
 urlpatterns = [
     path("", search, name='search'),
+    path("bookmark", bookmark, name='make_bookmark'),
 ]
--- user_actions/views.py
+++ user_actions/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from django.contrib import messages
 from django.contrib.postgres.search import TrigramSimilarity
 from .forms import SearchForm
-from reader.models import Chapter
+from django.contrib.auth.decorators import login_required
+from reader.models import Chapter, Manga
+from django.views.decorators.http import require_POST
+from account.models import Bookmark
+from django.http import Http404, JsonResponse
 
+@require_POST
 def search(request):
     form = SearchForm(request.POST)
     if form.is_valid():
@@ -25,3 +31,24 @@ def search(request):
             },
         )
     return render(request, "user_actions/search_results.html", {"empty": True})
+
+@require_POST
+@login_required
+def bookmark(request):
+    chapter_slug = request.POST['chapter_slug']
+    manga_slug = request.POST['manga_slug']
+    try:
+        manga = Manga.objects.get(slug=manga_slug)
+        chapter = Chapter.objects.get(name=chapter_slug)
+    except:
+        raise Http404()
+    if manga_slug and chapter_slug:
+        previous_bookmark = Bookmark.objects.filter(manga=manga, profile=request.user.profile)
+        if previous_bookmark:
+            previous_bookmark[0].chapter = chapter
+            previous_bookmark[0].save()
+        else:
+            Bookmark.objects.create(manga=manga, profile=request, chapter=chapter)
+        return JsonResponse({'status': 'ok'})
+    else:
+        raise Http404(_('Manga and chapter required parameters'))
