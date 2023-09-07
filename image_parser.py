b438e1f7bf8fe52caef5ec40bf872b0624537224
Alan Yuldashev <alanuldashev@gmail.com>
2023-09-07 21:43:47 +0600

    image parser update

--- image_parser.py
+++ image_parser.py
@@ -3,6 +3,9 @@ import random, bs4, os, time
 import slugify
 import logging, django, re
 from django.conf import settings
+from typing import List
+from requests import Response
+from image_size_reducer import image_reducer
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "manga_reader.settings")
 django.setup()
 # settings.configure()
@@ -31,27 +34,28 @@ headers = {
     "cookie": "_gid=GA1.2.1125372524.1692948422; zone-cap-5050238=1%3B1692948427; _ga_1EBSXM5LQM=GS1.1.1692948422.3.1.1692948428.0.0.0; _ga=GA1.1.1727617779.1692773463",
 }
 main_folder = "./imgs"
-
-
-def get_page(url):
+manga_name = 'Attack on titan'
+img_selector = '.text-center img'
+def get_page(url:str):
     response = request(url=url, headers=headers, method="GET")
     return response
 
 
-def get_img_in_ch(url):
+def get_img_in_ch(url:str):
     resp = get_page(url)
     soup = bs4.BeautifulSoup(resp.text, "html.parser")
-    imgs = soup.select("p img")
+    imgs = soup.select(img_selector)
     links = [img.get("src") for img in imgs]
     return links
 
 
-def write_into_file_and_save(response, path_to_save, path_for_django, chapter_instance):
+def write_into_file_and_save(response: Response, path_to_save, path_for_django, chapter_instance):
     if not os.path.exists(path_to_save):
         with open(path_to_save, "wb") as file:
             file.write(response.content)
             print(".", end="", flush=True)
-    pic = Picture.objects.get_or_create(chapter=chapter_instance, img=path_for_django)
+        pic = Picture.objects.get_or_create(chapter=chapter_instance, img=path_for_django)
+        image_reducer(picture_inst=pic[0])
     logging.info('already downloaded')
 
 
@@ -76,11 +80,10 @@ def download_chapter_imgs(urls, output_folder, chapter_instance):
             logging.warning(f"failed to download")
 
 
-def get_chapter_links(amount="all"):
-    url = "https://stone-dr.com/"
+def get_chapter_links(url:str, selector:str, amount:int = "all") -> List[str]:
     response = get_page(url)
     soup = bs4.BeautifulSoup(response.text, "html.parser")
-    raw_ch_links = soup.select(".ceo_latest_comics_widget ul li a")
+    raw_ch_links = soup.select(selector)
     if amount == "all":
         till = len(raw_ch_links)
     else:
@@ -92,17 +95,37 @@ def get_chapter_links(amount="all"):
 def create_chapter(chapter_name):
     match = re.search(pattern=r'\d+', string=chapter_name)
     if match:
-        chapter_number = match.group()
-    manga_inst = Manga.objects.filter(name='Dr Stone')[0]
+        chapter_number = int(match.group())
+    manga_inst = Manga.objects.filter(name=manga_name)[0]
     return Chapter.objects.get_or_create(name=chapter_name, manga=manga_inst, chapter_number=chapter_number)
 
-
-if __name__ == "__main__":
-    ## do not forget about amount and -10 in get_Chapter_link
-    for link in get_chapter_links(amount='all'):
+def create_loop(chapter_links, site_url_split):
+    for link in chapter_links:
         links = get_img_in_ch(link)
-        folder_name = link.split("https://stone-dr.com/manga/dr-stone-")[1][:-1]
+        folder_name = link.split(site_url_split)[1]
         print(folder_name)
         chapter_instance = create_chapter(folder_name)[0]
         download_chapter_imgs(links, folder_name, chapter_instance)
     logging.info("Success")
+
+def main_parser(url:str, selector:str, site_url_split:str, amount:int='all') -> None:
+    chapter_links = get_chapter_links(url=url, selector=selector, amount=amount)
+    print(chapter_links)
+    create_loop(chapter_links, site_url_split)
+
+if __name__ == "__main__":
+    ## do not forget about amount and -10 in get_Chapter_link
+    # url1 = "https://stone-dr.com/"
+    # selector1 = ".ceo_latest_comics_widget ul li a"
+    # site_url_split = "https://stone-dr.com/manga/dr-stone-"
+    selector = '.bg-bg-secondary .grid .col-span-3 a'
+    url = "https://ww8.readsnk.com/manga/shingeki-no-kyojin/"
+    site_url_split = "https://ww8.readsnk.com/chapter/"
+
+    main_parser(url=url, selector=selector, site_url_split=site_url_split)
+
+
+
+
+
+
