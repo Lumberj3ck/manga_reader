647b66cd530ed9854264ec5ec4c1eb3bccfa8d3c
Lumberj3ck <alanuldashev@gmail.com>
2023-10-24 08:44:48 +0000

    production changes

--- manga_reader/settings.py
+++ manga_reader/settings.py
@@ -76,7 +76,7 @@ PARLER_LANGUAGES = {
 CACHES = {
     "default": {
         "BACKEND": 'django_redis.cache.RedisCache',
-        "LOCATION": "redis://127.0.0.1:6379",
+        "LOCATION": "redis://0.0.0.0:6379",
     }
 }
 MIDDLEWARE = [
